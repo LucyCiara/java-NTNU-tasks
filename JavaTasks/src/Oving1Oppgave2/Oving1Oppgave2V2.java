c8bdd2250dbc30fdedfcba16bf7cb459a5c659e7
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2025-08-26 13:00:10 +0200

    Code cleanup and commenting
    I cleaned up the code and deleted a library zip file.
    I also added comments.

--- JavaTasks/src/Oving1Oppgave2/Oving1Oppgave2V2.java
+++ JavaTasks/src/Oving1Oppgave2/Oving1Oppgave2V2.java
@@ -3,24 +3,23 @@ package Oving1Oppgave2;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Dictionary;
 import java.util.HashMap;
-// import com.opencsv.CSVReader;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Oving1Oppgave2V2 {
     public static void main(String[] args) throws Exception {
-
+        // Finds the file path for the dataset CSV file.
         String filePath = new File("").getAbsolutePath() + "/data/time.csv";
 
+        // Creates a buffered reader based on the dataset's CSV file, which is then converted into a formated Map using a dedicated method.
         BufferedReader timeReader = new BufferedReader(new FileReader(filePath));
         Map<String, List<Double>> CSVFileMap = readerCSVFormater(timeReader);
+
+        // A for loop, which iterates through each column to count the total seconds and output them in the terminal.
         List<String> keyList = new ArrayList<>(CSVFileMap.keySet());
         for(int i = 0; i < CSVFileMap.get(keyList.get(0)).size(); i++){
             Double totalSecondCalc = 0.0;
@@ -33,16 +32,20 @@ public class Oving1Oppgave2V2 {
     }
 
     public static Map<String, List<Double>> readerCSVFormater(BufferedReader reader) throws IOException {
+        // Defines the Map which will be returned at the end of the method.
         Map<String, List<Double>> CSVFileMap = new HashMap<String, List<Double>>();
+        // Defines a nested List, which will keep the unformated information after it's converted to Doubles.
         List<List<Double>> lineNestList = new ArrayList<>();
-        String[] lineArray;
+        // Defines a List of what will become the CSVFileMap's keys.
         List<String> keyStrings = new ArrayList<>();
+        // Defines a List of Doubles, which will contain the information of each line converted into Doubles.
         List<Double> lineDoubleList = new ArrayList<>();
+
+        // A while loop which adds the necessary information (converted into Doubles) to lineNestList if it's possible to convert into a Double, and adds it to keyStrings if it can't (like if it's the names of each column, like at the top row).
         String line = new String();
         while((line = reader.readLine()) != null){
             try{
-                System.out.println(line);
-                lineArray = line.split(",");
+                String[] lineArray = line.split(",");
                 for (int i = 0; i < lineArray.length; i++){
                     lineDoubleList.add(Double.parseDouble(lineArray[i]));
                 }
@@ -53,6 +56,9 @@ public class Oving1Oppgave2V2 {
             }
 
         }
+        // A for loop which creates the CSVFileMap from the information in the lineNestList and the keyStrings.
+        // I realise that the way I formated it might be ill fitted for the exact task, but it's a pretty standard CSV format, which allows for keys based on the top line.
+        // If I were to make less universal, but more fitted code in the readerCSVFormater method, I might have kept the file information as a nested List.
         for(int i = 0; i < lineNestList.get(0).size(); i++){
             List<Double> timeUnits = new ArrayList<>();
             for(int j = 0; j < lineNestList.size(); j++){
@@ -61,6 +67,7 @@ public class Oving1Oppgave2V2 {
             CSVFileMap.put(keyStrings.get(i), new ArrayList<Double>(timeUnits));
             timeUnits.clear();
         }
+        // Returns a formated Map of the CSV file's information.
         return CSVFileMap;
     }
 }
